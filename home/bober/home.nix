b85a69ca2601a19a041eddfa272c42190e296232
bober <lubiebobea@gmail.com>
2025-06-08 20:58:44 +0200

    debil v3

--- home/bober/home.nix
+++ home/bober/home.nix
@@ -5,12 +5,23 @@ _: {
     stateVersion = "25.11";
   };
 
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "bira";
-      plugins = [ "git" ]; 
-    };
+programs.neovim = {
+  enable = true;
+ };
+
+
+
+
+programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  autosuggestion.enable = true;
+  syntaxHighlighting.enable = true;
+  oh-my-zsh.theme = "bira";
+  shellAliases = {
+    ll = "ls -l";
+    update = "sudo nixos-rebuild switch";
   };
+  history.size = 10000;
+};
 }
