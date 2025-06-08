ec72eee9716b9b148bc851f61ec5669bef269ab1
bober <lubiebobea@gmail.com>
2025-06-08 15:48:23 +0200

    zzzz

--- home/bober/home.nix
+++ home/bober/home.nix
@@ -5,5 +5,12 @@ _: {
     stateVersion = "25.11";
   };
 
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "bira";
+      plugins = [ "git" ]; 
+    };
+  };
 }
