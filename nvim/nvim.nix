927fdef8e7ea4f5c4d9326c70e7649310b89ee57
polskibobea <polskibobea@gmail.com>
2025-06-30 21:21:40 +0200

    sdad

--- nvim/nvim.nix
+++ nvim/nvim.nix
@@ -9,14 +9,17 @@
       name = "gruvbox";
       style = "dark";
     };
+
     plugins = {
-    statusline.lualine.enable = true;
-    telescope.enable = true;
-    autocomplete.nvim-cmp.enable = true;
- };
+      statusline.lualine.enable = true;
+      telescope.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+    };
+
     languages = {
       enableLSP = true;
       enableTreesitter = true;
+
       nix.enable = true;
       ts.enable = true;
       rust.enable = true;
